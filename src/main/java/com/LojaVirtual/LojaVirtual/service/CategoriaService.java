3bd97d761728ee1cc706057b60008e985d6d6f9f
Pedro Kusiak <pedro01kusiak@gmail.com>
2023-11-14 23:56:30 -0300

    Paginação de Categoria

--- src/main/java/com/LojaVirtual/LojaVirtual/service/CategoriaService.java
+++ src/main/java/com/LojaVirtual/LojaVirtual/service/CategoriaService.java
@@ -5,9 +5,12 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.LojaVirtual.LojaVirtual.entity.Categoria;
+import com.LojaVirtual.LojaVirtual.entity.Estado;
 import com.LojaVirtual.LojaVirtual.repository.CategoriaRepository;
 
 
@@ -18,8 +21,8 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
-    public List<Categoria> buscarTodos() {
-        return categoriaRepository.findAll();
+    public Page<Categoria> listarTodos(Pageable pageable) {
+        return categoriaRepository.findAll(pageable);
     }
 
     public Categoria buscarPorId(Long id) {
